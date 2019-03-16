8f5c7d2aab3f375b7f5e69925e4698e7f8378aa5
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-03-16 00:15:49 -0500

    created dog object, set it's name, breed, size and made it bark

--- src/chapter_2/page_36/DogTestDrive.java
+++ src/chapter_2/page_36/DogTestDrive.java
@@ -4,9 +4,15 @@ public class DogTestDrive {
 	public static void main (String[] args) {
 		Dog d = new Dog();
 		d.size = 40;
+		d.breed = "German Shepard";
+		d.name = "Ruffles";
 		d.bark();
 
-		System.out.println("My size is: " + d.size);
+		System.out.println();
+		System.out.println("----- Dog Info -----");
+		System.out.println("Name: " + d.name);
+		System.out.println("Breed: " + d.breed);
+		System.out.println("Size: " + d.size);
 
 	}
 }
