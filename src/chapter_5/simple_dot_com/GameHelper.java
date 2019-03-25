8e91794363bbba9e5ee6c6507dcedc9265a10c07
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-03-25 14:48:39 -0500

    refactored code to use Scanner utility in GameHelper class

--- src/chapter_5/simple_dot_com/GameHelper.java
+++ src/chapter_5/simple_dot_com/GameHelper.java
@@ -1,20 +1,27 @@
 package chapter_5.simple_dot_com;
 
 import java.io.*;
+import java.util.*;
 
 public class GameHelper {
     public String getUserInput (String prompt) {
-        String inputLine = null;
+//        String inputLine = null;
         System.out.print(prompt + " ");
 
-        try {
-            BufferedReader is = new BufferedReader(
-            new InputStreamReader(System.in));
-            inputLine = is.readLine();
-            if (inputLine.length() == 0) return null;
-        } catch (IOException e) {
-            System.out.println ("IOException: " + e);
-            }
+        Scanner sc = new Scanner(System.in);
+        String inputLine = sc.next();
+        if(inputLine.length() == 0) {
+            return null;
+        }
+
+//        try {
+//            BufferedReader is = new BufferedReader(
+//            new InputStreamReader(System.in));
+//            inputLine = is.readLine();
+//            if (inputLine.length() == 0) return null;
+//        } catch (IOException e) {
+//            System.out.println ("IOException: " + e);
+//            }
 
         return inputLine;
     } // close method
